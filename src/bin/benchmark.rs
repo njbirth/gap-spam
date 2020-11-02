d0412e0085e7664796340265b300dbf6445d91dc
Niklas Birth <birth@posteo.de>
2020-11-02 23:52:48 +0100

    - added benchmark.rs

---
+++ src/bin/benchmark.rs
@@ -0,0 +1,11 @@
+use gaps_rs::opt;
+use gaps_rs::Stats;
+
+fn main() {
+    println!("Hello World!");
+}
+
+// runs gaps, nwk and rfdist binaries and returns a stats struct (with valid rfdist)
+fn get_stats(opt: opt::Gaps) -> Stats {
+    unimplemented!()
+}
